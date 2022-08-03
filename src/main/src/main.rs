51f26f4760d6b683d29c648e9cf1388b06bea8cb
93nezyr <julen5raqukm@gmail.com>
2022-08-04 02:28:30 +0900

    Add CLI option
    1行ごとにドドスコさせるか、一度に全てのドドスコを行うかを選べるようにしました.

--- src/main/src/main.rs
+++ src/main/src/main.rs
@@ -1,7 +1,17 @@
 use dodosuko::tano_singo;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// tano_singoを一行ごとにドドスコさせるか、一度に全ドドスコさせるかのフラグ.
+    #[clap(long)]
+    pub is_new_line: bool,
+}
 
 fn main() {
     // println!("Hello, world!");
+    let args = Args::parse();
+    println!("{}", args.is_new_line);
 
-    tano_singo(true).unwrap();
+    tano_singo(args.is_new_line).unwrap();
 }
