0a22231fc3592c45fc6c2ffd81226dd27fde5ceb
93nezyr <julen5raqukm@gmail.com>
2022-08-04 02:42:41 +0900

    Update comment

--- src/main/src/main.rs
+++ src/main/src/main.rs
@@ -1,9 +1,14 @@
-use dodosuko::tano_singo;
 use clap::Parser;
+use dodosuko::tano_singo;
 
 #[derive(Debug, Parser)]
+/// # About
+/// 
+/// コマンドライン引数を受け付ける構造体です.
 struct Args {
     /// tano_singoを一行ごとにドドスコさせるか、一度に全ドドスコさせるかのフラグ.
+    /// 
+    /// trueなら、一行ごとにドドスコする.
     #[clap(long)]
     pub is_new_line: bool,
 }
