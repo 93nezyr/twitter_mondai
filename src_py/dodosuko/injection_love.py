3cd5618fcfc5d42e4c6f83495dc44a03dc67f4cc
93nezyr <julen5raqukm@gmail.com>
2022-08-14 03:33:24 +0900

    WIP: refactor friend's code

--- src_py/dodosuko/injection_love.py
+++ src_py/dodosuko/injection_love.py
@@ -11,3 +11,23 @@ def tano_singo():
             res = res + "ラブ注入♡"
             break
     print(res)
+
+def loidy_tano_singo() -> None:
+    """loidyが実装したドドスコプログラムをリファクタする場所です.
+
+    ## WIP
+    TODO: リファクタリング
+    """
+    box = ["ドド", "スコ"]
+    x = 0
+    res = ""
+    while x != 2:
+        for n in range(9):
+            x = random.randint(0, 1)
+            res = res + box[x]
+        print(res)
+        if res == "ドドスコスコスコドドスコスコスコドドスコスコス":
+            break
+        else:
+            res = ""
+    print("ラブ注入♡")
